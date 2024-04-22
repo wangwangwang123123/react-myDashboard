07c73eb2922393c5f084bc3d8dbd4a17b2be95b3
MengchaoWang <894421826@qq.com>
2024-04-22 17:13:28 +1000

    first one

---
+++ src/utilities/get-name-initials.ts
@@ -0,0 +1,11 @@
+
+
+// 获得首字母大写
+export const getNameInitials = (name: string, count = 2) => {
+  const initials = name
+    .split(" ")
+    .map((n) => n[0])
+    .join("");
+  const filtered = initials.replace(/[^a-zA-Z]/g, "");
+  return filtered.slice(0, count).toUpperCase();
+};
